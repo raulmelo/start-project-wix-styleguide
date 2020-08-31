1b21ab03f256e70c2a3a696a786046cde6d5146f
Raul Melo <raulmelo.web@gmail.com>
2020-08-31 00:22:54 -0300

    initial repositorio

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Avatar } from 'wix-style-react'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Avatar
+          imgProps={{ src: 'https://randomuser.me/api/portraits/women/39.jpg' }}
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
